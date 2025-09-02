a4040c005b77250cf2a68c5faf432d5515ef3d9b
Daniel Kazemian <achilles.7733@gmail.com>
2025-09-02 20:14:39 +0300

    Adding model & try to seprate files

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from db import db
 
-# Making a class & the model
+
 class Book(db.Model):
     id: int = db.Column(db.Integer, primary_key=True)
     author_name: str = db.Column(db.String(100), nullable=False)
@@ -9,6 +10,21 @@ class Book(db.Model):
 
     def __repr__(self) -> str:
         return f"<Book {self.book_name} by {self.author_name}>"
+
+
+class User(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True)
+    email: str = db.Column(db.String(200), unique=True, nullable=False)
+    password_hash: str = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Email is {self.email} and password is {self.password}>"
+
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
 
 
 # books: list[dict[str, str]] = [
